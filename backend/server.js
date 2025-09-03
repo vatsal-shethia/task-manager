0cc1af291d80463d37137b29e8cade60b327f01c
Vatsal <vatsalshethia19@gmail.com>
2025-09-03 12:06:51 +0530

    feat:setup frontend and backend

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+// Middleware to handle CORS
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+// Middleware
+app.use(express.json());
+
+// Routes(for API routes)
+// app.use("/api/auth", authRoutes);
+// app.use("/api/users", userRoutes);
+// app.use("/api/tasks", taskRoutes);
+// app.use("/api/reports", reportRoutes);
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
